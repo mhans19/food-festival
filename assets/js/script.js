cf8725ed1b60b239cbf0a4357b03198a386e923e
mhans19 <mdhansen19@gmail.com>
2020-09-02 21:40:50 -0600

    troubleshooting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-require('bootstrap')
+require("bootstrap");
 const img1 = require("../../assets/img/food-table.jpg");
 const img2 = require("../../assets/img/grill.jpg");
 
-$(document).ready(function() {
+$(document).ready(function () {
   // First image is hard coded in index.html
   const carouselSlides = [
     {
@@ -18,11 +18,11 @@ $(document).ready(function() {
       img: img2,
       btnText: "Purchase Tickets",
       btnUrl: "tickets.html"
-    },
-  ]
+    }
+  ];
 
   carouselSlides.forEach((slide, i) => {
-    $('.carousel-inner').append(`
+    $(".carousel-inner").append(`
   <div class="carousel-item fullscreen-carousel" style="background-image: url('${slide.img}')">
     <div class="d-flex h-100 align-items-center justify-content-center carousel-caption">
         <div class="container">
@@ -39,6 +39,6 @@ $(document).ready(function() {
           </div>
         </div>
     </div>
-  </div>`)
-  })
+  </div>`);
+  });
 });
